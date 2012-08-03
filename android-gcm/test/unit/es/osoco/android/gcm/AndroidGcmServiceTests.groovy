8652a4ccd58efb06ede4de51f19efc8877f0d436
deigote <diego@toharia.com>
2012-08-03 09:58:14 +0200

    Main android gcm service, probably should be renamed :)

---
+++ android-gcm/test/unit/es/osoco/android/gcm/AndroidGcmServiceTests.groovy
@@ -0,0 +1,17 @@
+package es.osoco.android.gcm
+
+import grails.test.*
+
+class AndroidGcmServiceTests extends GrailsUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
