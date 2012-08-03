f1ae2d26ec0d29ccd5cfc27b57258f9d25b1cefc
Adrián Santalla <adrian.santalla@osoco.es>
2012-08-03 13:54:43 +0200

    Added the necessary classes to manage the GCM connections.

--- android-gcm-client-example/src/es/osoco/Main.java
+++ android-gcm-client-example/src/es/osoco/Main.java
@@ -1,6 +1,8 @@
 package es.osoco;
 
 import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class Main extends Activity {
@@ -10,6 +12,12 @@ public class Main extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.main);
+
+        Intent registrationIntent = new Intent("com.google.android.c2dm.intent.REGISTER");
+        registrationIntent.putExtra("app", PendingIntent.getBroadcast(this, 0, new Intent(), 0));
+        registrationIntent.putExtra("sender", "1073974434973");
+        startService(registrationIntent);
     }
 }
