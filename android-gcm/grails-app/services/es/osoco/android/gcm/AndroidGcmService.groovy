f1df149fba30fb2891ec9a3e681cc1f1750adb8b
Adrián Santalla <adrian.santalla@osoco.es>
2012-08-03 10:51:21 +0200

    Added a method to send one message per device.

--- android-gcm/grails-app/services/es/osoco/android/gcm/AndroidGcmService.groovy
+++ android-gcm/grails-app/services/es/osoco/android/gcm/AndroidGcmService.groovy
@@ -1,11 +1,24 @@
 package es.osoco.android.gcm
 
+import com.google.android.gcm.server.Message
+import com.google.android.gcm.server.Result
+import com.google.android.gcm.server.Sender
+
 class AndroidGcmService {
 
-    static transactional = true
-	def grailsApplication 
+    static transactional = false
+
+	def grailsApplication
+
+    def Result sendMessage(Message message, java.lang.String registrationId, int retries) {
+        sender().send(message, registrationId, retries)
+    }
+
+    private sender() {
+        new Sender(getApiKey());
+    }
 
     private getApiKey() {
-		grailsApplication.config.android.gcm.api.key
+        grailsApplication.config.android.gcm.api.key
     }
 }
