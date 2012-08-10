b3e9a835198c5a59f72ba7baea834a512ac2559c
deigote <diego@toharia.com>
2012-08-10 12:23:10 +0200

    Added a message that dinamycally composes the appropiate send method call and invokes it

--- android-gcm/grails-app/services/es/osoco/android/gcm/AndroidGcmService.groovy
+++ android-gcm/grails-app/services/es/osoco/android/gcm/AndroidGcmService.groovy
@@ -1,5 +1,8 @@
 package es.osoco.android.gcm
 
+import java.util.List;
+import java.util.Map;
+
 import com.google.android.gcm.server.Message
 import com.google.android.gcm.server.MulticastResult
 import com.google.android.gcm.server.Result
@@ -13,6 +16,23 @@ class AndroidGcmService {
 
 	def grailsApplication
 
+	/**
+	 * Sends a message by invoking the appropriate send method depending on the parameters
+	 * @param data The message
+	 * @param registrationIds The devices registration ids
+	 * @param collapseKey the collapse key (by default is empty)
+	 * @param multicast whether the message should be sent as a multicast message or not 
+	 * (by default it will be true if more than one registration id is passed)  
+	 * @return
+	 */
+	def sendMessage(Map data, List<String> registrationIds, String collapseKey = '', boolean multicast = registrationIds.size() > 0)
+	{
+		def method = "send${multicast ? 'Multicast' : ''}${collapseKey ? 'Collapse' : 'Instant'}Message"
+		def params = (collapseKey ? [collapseKey] : []) + [data] + (multicast ?
+			[registrationIds] : [registrationIds[0]])
+		this.invokeMethod(method, params.toArray())
+	}
+		
     def Result sendCollapseMessage(String collapseKey, Map data, String registrationId) {
         sender().send(buildCollapseMessage(collapseKey, data), registrationId, retries())
     }
@@ -30,19 +50,19 @@ class AndroidGcmService {
     }
 
     private Message buildCollapseMessage(String collapseKey, Map data) {
-        Message message = new Message.Builder()
+        Message.Builder messageBuilder = new Message.Builder()
             .collapseKey(collapseKey)
             .timeToLive(timeToLive())
             .delayWhileIdle(delayWhileIdle())
 
         data.each {
-            message.addData(it.key, it.value)
+            messageBuilder.addData(it.key, it.value)
         }
 
-        message.build()
+        messageBuilder.build()
     }
 
-    private Message buildInstantMessage(String data) {
+    private Message buildInstantMessage(Map data) {
         Message message = new Message.Builder()
             .delayWhileIdle(delayWhileIdle())
 
