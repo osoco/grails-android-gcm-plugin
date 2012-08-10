424a8cec46bab80faf20f302955cec8bd7e313d1
Adrián Santalla <adrian.santalla@osoco.es>
2012-08-10 13:33:42 +0200

    Applied the ActionBar style.

--- android-gcm-client-example/src/es/osoco/gcmtester/Main.java
+++ android-gcm-client-example/src/es/osoco/gcmtester/Main.java
@@ -11,6 +11,9 @@ public class Main extends SherlockFragmentActivity {
 
         setContentView(R.layout.main);
 
+        getSupportActionBar().setIcon(R.drawable.logo);
+        getSupportActionBar().setTitle(R.string.ab_title);
+
 //        Intent registrationIntent = new Intent("com.google.android.c2dm.intent.REGISTER");
 //        registrationIntent.putExtra("app", PendingIntent.getBroadcast(this, 0, new Intent(), 0));
 //        registrationIntent.putExtra("sender", "1073974434973");
